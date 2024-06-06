0444f1549631d043daf482f47a8335b9b41eac5f
Tobias Brasser <tbr142625@stud.gibb.ch>
2024-06-06 15:21:04 +0200

    added deploy and predeploy

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "92c39a349ab96745"; }
+/******/ 		__webpack_require__.h = function() { return "2d449b43169d09f1"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
